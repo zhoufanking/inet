b88f69017e1bbc39c44198632d8d8b2575d142b2
Levente Mészáros <levy@omnetpp.org>
2015-07-10 17:28:02 +0200

    Test: More precise reporting for fingerprint evolution diff.

--- tests/fingerprint/diffingerprints.sh
+++ tests/fingerprint/diffingerprints.sh
@@ -9,9 +9,12 @@
 # record-eventlog = true
 # fingerprint = 0000-0000
 
-grep "E #" $1 | awk '{printf("# %s f %08x\n", $3, $13);}' > $1.tmp
-grep "E #" $2 | awk '{printf("# %s f %08x\n", $3, $13);}' > $2.tmp
-diff -U 1 $1.tmp $2.tmp > diffingerprints.diff
+grep "E #" $1 | awk '{print $13}' | uniq > $1.tmp
+grep "E #" $2 | awk '{print $13}' | uniq > $2.tmp
+diff -y -t --suppress-common-lines $1.tmp $2.tmp > diffingerprints.diff
 rm $1.tmp
 rm $2.tmp
-head -n 10 diffingerprints.diff
+f1=`head -n 1 diffingerprints.diff | awk '{print $1}'`
+f2=`head -n 1 diffingerprints.diff | awk '{print $3}'`
+grep -e "$f1" $1 | head -n 1
+grep -e "$f2" $2 | head -n 1
