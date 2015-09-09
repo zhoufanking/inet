00e8f6bbbbedbdbe8e02aa3f64c11b91b87d99d0
Andras Varga <andras@omnetpp.org>
2015-09-09 17:34:40 +0200

    xxxxx

--- src/inet/linklayer/newmac/Ieee80211MacTransmission.h
+++ src/inet/linklayer/newmac/Ieee80211MacTransmission.h
@@ -28,16 +28,17 @@ class Ieee80211MacTransmission;
 
 class IIeee80211Tx {
     public:
-    void transmitContentionFrame(Ieee80211Frame *frame, simtime_t ifs, simtime_t eifs, int cwMin, int cwMax, int retryCount, ITransmissionCompleteCallback *transmissionCompleteCallback); // explicit ifs, eifs, cwMin, cwMax
-    void mediumStateChanged(bool mediumFree);
-    void transmissionStateChanged(IRadio::TransmissionState transmissionState);
-    void lowerFrameReceived(bool isFcsOk);
+        class ICallback {
+            public:
+               virtual void transmissionComplete(IIeee80211Tx *tx) = 0; // tx=nullptr if frame was transmitted by MAC itself (immediate frame!), not a tx process
+        };
+
+        virtual void transmitContentionFrame(Ieee80211Frame *frame, simtime_t ifs, simtime_t eifs, int cwMin, int cwMax, int retryCount, ITransmissionCompleteCallback *transmissionCompleteCallback) = 0;
+        virtual void mediumStateChanged(bool mediumFree) = 0;
+        virtual void transmissionStateChanged(IRadio::TransmissionState transmissionState) = 0;
+        virtual void lowerFrameReceived(bool isFcsOk) = 0;
 };
 
-class ITransmissionCompleteCallback {  //or ITransmissionListener?
-    public:
-       virtual void transmissionComplete(Ieee80211MacTransmission *tx) = 0; //tx=nullptr if frame was transmitted by MAC itself (immediate frame!), not a tx process
-};
 
 //TODO EDCA internal collisions should trigger retry (exp.backoff) in the lower pri tx process(es)
 class Ieee80211MacTransmission : public Ieee80211MacPlugin
