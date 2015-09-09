3764fbda629fa6298bcd0fdec069613e54cba812
Zoltan Bojthe <zbojthe@omnetpp.org>
2015-09-09 14:54:32 +0200

    hierarchical99 fixes (Flood, WiseRoute, Probabilistic networklayers)
    SimpleNetworkLayer and SimpleNetworkProtocolControlInfo removed

--- src/inet/networklayer/flood/Flood.cc
+++ src/inet/networklayer/flood/Flood.cc
@@ -21,10 +21,11 @@
  **************************************************************************/
 
 #include "inet/networklayer/flood/Flood.h"
+
+#include "inet/linklayer/common/SimpleLinkLayerControlInfo.h"
 #include "inet/networklayer/contract/IL3AddressType.h"
 #include "inet/networklayer/contract/INetworkProtocolControlInfo.h"
-#include "inet/linklayer/common/SimpleLinkLayerControlInfo.h"
-#include "inet/networklayer/common/SimpleNetworkProtocolControlInfo.h"
+#include "inet/networklayer/contract/generic/GenericNetworkProtocolControlInfo.h"
 
 namespace inet {
 
@@ -247,7 +248,7 @@ bool Flood::notBroadcasted(FloodDatagram *msg)
  **/
 cMessage *Flood::decapsMsg(FloodDatagram *floodDatagram)
 {
-    SimpleNetworkProtocolControlInfo *controlInfo = new SimpleNetworkProtocolControlInfo();
+    GenericNetworkProtocolControlInfo *controlInfo = new GenericNetworkProtocolControlInfo();
     controlInfo->setSourceAddress(floodDatagram->getSourceAddress());
     controlInfo->setProtocol(floodDatagram->getTransportProtocol());
     cPacket *transportPacket = floodDatagram->decapsulate();
@@ -304,6 +305,7 @@ cObject *Flood::setDownControlInfo(cMessage *const pMsg, const MACAddress& pDest
 {
     SimpleLinkLayerControlInfo *const cCtrlInfo = new SimpleLinkLayerControlInfo();
     cCtrlInfo->setDest(pDestAddr);
+    cCtrlInfo->setProtocol(ETHERTYPE_INET_GENERIC);
     pMsg->setControlInfo(cCtrlInfo);
     return cCtrlInfo;
 }
