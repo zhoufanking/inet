849753eca1d08484f763ca959d68c5650fe18866
Andras Varga <andras@omnetpp.org>
2015-09-11 13:04:01 +0200

    rename MacContext to UpperMacContext

--- src/inet/linklayer/newmac/Ieee80211UpperMac.cc
+++ src/inet/linklayer/newmac/Ieee80211UpperMac.cc
@@ -3,21 +3,21 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 #include "Ieee80211UpperMac.h"
 #include "Ieee80211NewMac.h"
 #include "IIeee80211MacTx.h"
 #include "IIeee80211MacImmediateTx.h"
-#include "IIeee80211MacContext.h"
+#include "IIeee80211UpperMacContext.h"
 #include "inet/common/ModuleAccess.h"
 #include "Ieee80211FrameExchanges.h"
 
