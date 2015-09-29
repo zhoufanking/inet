0c09c6ce2dc5cf48ee7bf92f43a2eb8feb277daa
Andras Varga <andras@omnetpp.org>
2015-09-29 15:05:22 +0200

    display string tweaks

--- src/inet/linklayer/ieee80211/newmac/BasicContentionTx.cc
+++ src/inet/linklayer/ieee80211/newmac/BasicContentionTx.cc
@@ -305,7 +305,10 @@ void BasicContentionTx::updateDisplayString()
     std::stringstream os;
     if (frame)
         os << frame->getName() << "\n";
-    os << fsm.getStateName();
+    const char *stateName = fsm.getStateName();
+    if (strcmp(stateName, "IFS_AND_BACKOFF") == 0)
+        stateName = "IFS&BKOFF";
+    os << stateName;
     getDisplayString().setTagArg("t", 0, os.str().c_str());
 }
 
