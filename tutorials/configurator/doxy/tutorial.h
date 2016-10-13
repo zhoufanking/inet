0f8a34b45419d5ae84236e9e7742f28bea04a1ea
Gyorgy Szaszko <adam.szaszko@gmail.com>
2016-10-13 12:03:48 +0200

    updates

--- tutorials/configurator/doxy/tutorial.h
+++ tutorials/configurator/doxy/tutorial.h
@@ -1,13 +1,15 @@
 /**
 @mainpage Configurator Tutorial for the INET Framework
 
-This tutorial will show how to use the IPv4NetworkConfigurator module to configure IP addresses and routing in wired and wireless IPv4 networks in the INET framework.
+This tutorial will show how to use the <tt>IPv4NetworkConfigurator</tt> module to configure IP addresses and routing in wired and wireless IPv4 networks in the INET framework.
 The tutorial is organized into multiple steps, each corresponding to a simulation model. The steps demonstrate how to accomplish certain
 tasks with the configurator.
 
 This is an advanced tutorial, and assumes that the reader is familiar with creating and running simulations in @opp and INET. If that wasn't the case,
-the TicToc Tutorial is a good starting point to get started with @opp. The INET Walkthrough is an introduction to INET and how to work with protocols.
-The Wireless Tutorial is another advanced tutorial, and it deals with wireless features of the INET framework.
+the <a href="https://omnetpp.org/doc/omnetpp/tictoc-tutorial/"
+target="_blank">TicToc Tutorial</a> is a good starting point to get started with @opp. The <a
+href="../../../doc/walkthrough/tutorial.html" target="_blank">INET Walkthrough</a> is an introduction to INET and how to work with protocols.
+The <a href="https://omnetpp.org/doc/inet/api-current/tutorials/wireless/" target="_blank">Wireless Tutorial</a> is another advanced tutorial, and deals with wireless features of the INET framework.
 
 For additional information, the following documentation should be useful:
 
@@ -32,11 +34,15 @@ defined in separate .NED files.
 @section s1goals Goals
 
 The goal of this step is to demonstrate that in many scenarios, the configurator can adequatelly configure the network with its default settings, without
-any user input. This is useful when it is irrelevant what the nodes' IP addresses are in a simulation, because the goal is to study wireless
-transmission ranges, for example. The default settings 'just work'.
+any user input. This is useful when it is irrelevant what the nodes' actual IP addresses are in a simulation, because the goal is to study wireless
+transmission ranges, for example. The default settings will result in a network where routing 'just works'.
+In this step, we will show that the configurator's automatic IP address assignment is adequate for the example network.
 
 @section s1model The model
 
 <strong>About the configurator</strong>
+
+In INET simulations, configurator modules are responsible for assigning IP addresses to network nodes, and for creating their
+routing tables. Essentially, the configurator simulates a real life network administrator.
 
 */
